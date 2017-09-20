8355fc676fabbf98be948c9e89cdd8bc09411c84
Vitaly <VitalyKob@users.noreply.github.com>
2017-09-20 17:30:54 -0400

    Create check_disk.py
    Check disk usage.

---
+++ check_disk.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#requires python 2.7
+
+import subprocess
+partition_usage_threshold = 20
+df_cmd = subprocess.check_output(['df','-k'])
+lines = df_cmd.splitlines()
+for line in lines[1:]:
+    columns = line.split()
+    used_percentage = columns[4]
+    used_percentage = used_percentage.replace('%','')
+    if int(used_percentage) >= partition_usage_threshold:
+        print "partition %s usage is beyond threshold at %s" % (columns[0], columns[4])
+        #function to send email 
