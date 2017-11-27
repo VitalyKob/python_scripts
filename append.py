4eae9c941ce8feb469ac3e528b63f336c41d0134
Vitaly <VitalyKob@users.noreply.github.com>
2017-11-27 14:53:20 -0500

    Update append.py

--- append.py
+++ append.py
@@ -10,3 +10,18 @@ while True:
     time.sleep(5)
 
 f.close()
+
+############################
+
+#!/usr/bin/python3
+
+import time
+#f = open("test.txt","w+")
+f = open("test.txt","a")
+
+for i in range(100):
+   f.write("This is line %d\r\n" % (i))
+   time.sleep(1)
+   i += 1
+
+f.close()
